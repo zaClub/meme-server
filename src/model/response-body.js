adfc12bfaa05342f430204b9902580138e8351db
AymaxLi <848478161@qq.com>
2018-04-28 23:15:52 +0800

    refactor(model): 修改 respond-body 的构造函数传参方式

--- src/model/response-body.js
+++ src/model/response-body.js
@@ -1,6 +1,14 @@
-module.exports = function ResBody(code, msg = '', data = {}) {
+/**
+ * 响应体构造函数
+ * @param {Number} code 错误码
+ * @param {String} msg 信息
+ * @param {Object} data 数据对象
+ */
+function ResBody({code, msg = 'asdfsdf', data = {}}) {
 
   this.code = code
   this.msg = msg
   this.data = data
 }
+
+module.exports = ResBody
